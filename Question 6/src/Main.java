cbb88bfbedd7ec0870360cf290ba0a4573291d94
HazeeqHaikal <haikalroslan740@gmail.com>
2023-07-06 12:47:31 +0800

    fix

--- Question 6/src/Main.java
+++ Question 6/src/Main.java
@@ -5,52 +5,45 @@ public class Main {
         Scanner strInput = new Scanner(System.in);
         Scanner intInput = new Scanner(System.in);
 
-        // Declare array of object
         System.out.print("Enter number of order: ");
         int orderNum = intInput.nextInt();
 
         DesignService[] ds = new DesignService[orderNum];
 
-        // Step 2; input
         for (int i = 0; i < orderNum; i++) {
             System.out.print("Enter Customer Name: ");
-            String cn = strInput.nextLine();
-            System.out.print("Enter Deposit: RM");
-            double dp = strInput.nextDouble();
-            System.out.print("Enter Urgent order(true/false) :");
-            boolean uo = strInput.nextBoolean();
+            String custName = strInput.nextLine();
+            System.out.print("Enter amount of deposit (RM): ");
+            double deposit = intInput.nextDouble();
+            System.out.println("Is this an urgent order? (true/false): ");
+            boolean urgentOrder = intInput.nextBoolean();
 
-            // ask for optiobn 1- physicalDesign or 2- digitalDesign
-            System.out.print("Enter Design Type (1-Physical/2-Digital): ");
+            System.out.print("1. Physical Design\n2. Digital Design\nEnter your choice: ");
             int choice = intInput.nextInt();
 
             if (choice == 1) {
-                // input specific
-                System.out.print("Enter Physical Type(1-Banner/2-Brochure/3-Poster/4-BusinessCArd):");
-                int pt = intInput.nextInt();
-                System.out.print("Enter Printing(true/false)");
+                System.out.print("1. Banner\n2. Brochure\n3. Poster\n4. BusinessCard\nEnter your choice: ");
+                int physicalType = intInput.nextInt();
+                System.out.print("Enter Printing (true/false): ");
                 boolean print = intInput.nextBoolean();
-                System.out.print("Enter number of Copy: ");
+                System.out.print("Enter number of copy: ");
                 int copy = intInput.nextInt();
 
-                // step 3;Store into objects
-                ds[i] = new PhysicalDesign(cn, dp, uo, pt, print, copy);
-            } else {
+                ds[i] = new PhysicalDesign(custName, deposit, urgentOrder, physicalType, print, copy);
+            } else if (choice == 2) {
                 System.out.print("Enter Digital Type: ");
-                int dt = intInput.nextInt();
+                int digitalType = intInput.nextInt();
 
-                ds[i] = new DigitalDesign(cn, dp, uo, dt);
+                ds[i] = new DigitalDesign(custName, deposit, urgentOrder, digitalType);
             }
 
         }
-        // manipulation
-        // -i)
-        System.out.print("Details of Physical Design order:\n\n");
+        System.out.println("Details of Physical Design order:\n\n");
         for (int i = 0; i < orderNum; i++) {
             if (ds[i] instanceof PhysicalDesign)
                 System.out.println(ds[i].toString() + "\nPAyment: RM" + ds[i].calcPayment());
         }
-        // -ii)
+
         int count = 0;
         for (int i = 0; i < orderNum; i++) {
             if (ds[i] instanceof DigitalDesign) {
